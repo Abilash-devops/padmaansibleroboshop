9a81dee8802a42fd0e5da3e52c2d2a188cc87ee3
Abilash-devops <abilashdevopsaws@gmail.com>
2023-09-03 16:53:22 +0530

    commited

--- terminate.sh
+++ terminate.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 instance_id=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=running | jq -r '.Reservations[].Instances[].InstanceId')
-for instance_ids in instance_id
-do
-aws ec2 terminate-instances --instance_ids
-done
+aws ec2 terminate-instances --instance-ids $instance_id
